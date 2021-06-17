8c48c27387c6814dd2b18982f300e75a76c44bee
Adam <raqsasg@gmail.com>
2021-06-18 01:34:24 +0200

    Remove spaces

--- oled.py
+++ oled.py
@@ -19,7 +19,7 @@ def signal_handler(sig, frame):
 # Check for arguments
 if(len(sys.argv) < 2):
 	print("Usage: oled.py image.gif\n")
-	sys.exit(0)        
+	sys.exit(0)
 
 # Set up ctrl-c handler
 signal.signal(signal.SIGINT, signal_handler)
@@ -45,14 +45,14 @@ im = Image.open(sys.argv[1])
 
 while(1):
 	for frame in ImageSequence.Iterator(im):
-	    
+
 	    # Image size based on Apex 5 and 7
 	    frame = frame.resize((128, 40))
-	    
+
 	    # Convert to monochrome
 	    frame = frame.convert('1')
 	    data = frame.tobytes()
-	    
+
 	    # Set up feature report package
 	    data = bytearray([0x61]) + data + bytearray([0x00])
 
